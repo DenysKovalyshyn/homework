6c2030efc39f19cd8ce1d30c0db00c53fb14a99d
DenysKovalyshyn <denys.kovalyshyn@skelia.com>
2022-02-14 14:09:43 +0200

    refactoring files

--- src/main/java/ProgramElements.java
+++ src/main/java/ProgramElements.java
@@ -1,8 +1,7 @@
 public class ProgramElements {
 
-    public String studentInfo() {
-        String fullName = "Denys Kovalyshyn";
-        String studentInfo = "The new student of the course \"QA Automation\" is %s";
-        return String.format(studentInfo, fullName);
+    public String studentInfo(String fullName, int age) {
+        String studentInfo = "The new student of the course \"QA Automation\" is %s. He is %d years old";
+        return String.format(studentInfo, fullName, age);
     }
 }
--- src/main/java/ProgramExecution.java
+++ src/main/java/ProgramExecution.java
@@ -1,7 +1,9 @@
 public class ProgramExecution {
 
     public static void main(String[] args) {
+        String fullName = "Denys Kovalyshyn";
+        int age = 32;
         ProgramElements programElements = new ProgramElements();
-        System.out.println(programElements.studentInfo());
+        System.out.println(programElements.studentInfo(fullName, age));
     }
 }
