cc1287786a08458b1a06ee56b3d21559177d858d
DenysKovalyshyn <denys.kovalyshyn@skelia.com>
2022-08-29 13:45:37 +0300

    lesson34

---
+++ src/main/java/lesson34/UserSession.java
@@ -0,0 +1,14 @@
+package lesson34;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserSession {
+    @JsonProperty("session_token")
+    public String sessionToken;
+
+    @JsonProperty
+    public String username;
+
+    @JsonProperty("valid_to")
+    public String validTo;
+}
